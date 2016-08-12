d7cb2a3aceb96210ecbd01bc6a5048eaeb09f5ab
Nicholas Porter <njporter10@gmail.com>
2016-08-12 15:44:39 -0700

    got the first letter path found but not the others

--- frontier.js
+++ frontier.js
@@ -18,8 +18,8 @@ function Path(){
 
 //           Nodes
 // has a LIST of other connected nodes and CONTENT,
-// it can search all descendant nodes to find a solition
-function Node(c){
+// it can search all descendant nodes to find a solution
+function Nodes(c){
     this.contents = c;
     this.children = [];
 }
@@ -32,7 +32,7 @@ function Node(c){
 //NOTE: ypu can modify the position of the position assignment to change the Search Strategy;
 
 function pickPath(f){
-    var position = o;
+    var position = 0;
     var ret = f[position];
     f.splice(position,1);
     return ret;
@@ -46,8 +46,9 @@ function pickPath(f){
 //EFFECT - outputs True if path contains GOAL
 
 function hasGoal(s,p){
+    console.log("p.contents: ", p.contents);
     for(i in p.contents){
-        if(p.contents[i] == s){
+        if(p.contents[i].contents == s){
             return true;
         }
         else{
@@ -65,8 +66,9 @@ function search(query,start){
     p.contents.push(start);
 
     frontier.push(p); //puts the new Path into the frontier
-
+    console.log("frontier Array: " , frontier);
     while(frontier.length > 0){
+        console.log("frontier: ", frontier );
         //select and remove a path from frontier
         pick = pickPath(frontier); //pickPath helper fn
 
@@ -79,13 +81,53 @@ function search(query,start){
             last = pick.contents[size - 1];
 
             for(n in last.children){
-                toAdd = newPath();
+                toAdd = new Path();
                 for(x in pick.contents){
-                    toAdd.contents.push(last.children[n]);
-                    frontier.push(toAdd);
+                    toAdd.contents.push(pick.contents[x]);
                 }
+                toAdd.contents.push(last.children[n]);
+                console.log("pushing to frontier array of nodes");
+                frontier.push(toAdd);
             }
         }
     }
     return false;
 }
+
+function printer(p){
+    if(p == false){
+        return "No Solution Found";
+    }
+    else{
+        var s = "Solution Found, Path!";
+        for(i = 0; i <p.contents.length ; i++){
+            s += p.contents[i].contents + " , ";
+        }
+        return s;
+    }
+}
+
+
+//DATA
+// creates and connects nodes a,b,c,d
+var a = new Nodes("a");
+var b = new Nodes("b");
+var c = new Nodes("c");
+var d = new Nodes("d");
+a.children.push(b);
+a.children.push(c);
+c.children.push(d);
+
+//test search 
+pa = search ('a', a);
+pc = search ('c', a);
+pd = search ("d", a);
+pg = search ('g', a);
+console.log('pa: ', printer(pa));
+console.log("pc: ", printer(pc));
+console.log("pd: ", printer(pd));
+console.log("pg: ", printer(pg));
+document.write('pa: ', printer(pa));
+document.write("pc: ", printer(pc));
+document.write("pd: ", printer(pd));
+document.write("pg: ", printer(pg));
